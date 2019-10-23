3052fabcee92684782a1ebfd9b247894b3c76943
D4ZC <39981417+D4ZC@users.noreply.github.com>
2019-10-23 15:30:07 -0500

    Add files via upload

---
+++ Proyecto PsicoTest/src/proyecto/psicotest/Ventanas/lblFoto.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.psicotest.Ventanas;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author DAZC9
+ */
+class lblFoto {
+
+    static void setText(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+  
+    
+    static void setIcon(Icon icono) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
